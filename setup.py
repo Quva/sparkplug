17a15195799311b3b40422566fdf8588a44d1488
terkkila <timo.erkkila@gmail.com>
2016-03-23 14:26:50 +0200

    Bumped version 2.0.1 -> 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup,find_packages
 
 setup( name         = 'sparkplug',
-       version      = '2.0.1',
+       version      = '2.0.2',
        description  = 'Sparkplug',
        author       = 'Quva Oy',
        url          = 'https://github.com/Quva/sparkplug.git',
