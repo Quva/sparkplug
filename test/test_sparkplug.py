510e2be5e56e6773b6dec4b3f161a30b2e25d123
terkkila <timo.erkkila@gmail.com>
2016-10-15 22:18:28 +0300

    Small tweaks.

--- test/test_sparkplug.py
+++ test/test_sparkplug.py
@@ -1,6 +1,6 @@
 
 import unittest
-from nose.tools import raises
+from nose.tools import raises, assert_equal
 
 from sparkplug import SparkPlug
 
@@ -12,16 +12,17 @@ class SparkPlugTest(unittest.TestCase):
         
         message = self.plug.loadJSON("test/test_event_v2.json")
         self.plug.validate(message)
-        
+
+    def test_event_message_v2_xml(self):
+
+        message = self.plug.loadXML("test/test_event_v2.xml")        
+        self.plug.validate(message)
+                
     def test_event_message_v2_no_data_json(self):
 
         message = self.plug.loadJSON("test/test_event_v2_no_data.json")        
         self.plug.validate(message)
         
-    def test_event_message_v2_xml(self):
-
-        message = self.plug.loadXML("test/test_event_v2.xml")        
-        self.plug.validate(message)
         
     def test_event_message_v2_no_data_xml(self):
         
