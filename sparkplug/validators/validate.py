b8c47e6f7ef3f316494bb6eb209fdf7f8b88db61
terkkila <timo.erkkila@gmail.com>
2017-03-31 00:27:06 +0300

    Loosened v1 event message validation. Added unit tests. Unit tests passing.

--- sparkplug/validators/validate.py
+++ sparkplug/validators/validate.py
@@ -5,11 +5,11 @@ from .schemas import Schemas
 
 
 def validateMessage(message):
-      
-      validateMessageWithSchema(message, Schemas.latest.getSchemaForMessageType("message"))
-      
+
+      validateMessageWithSchema(message, Schemas.latest.getSchemaForMessageType("message"), "latest")
+
       header = message["message_header"]
-      
+
       ver = header.get("message_version", "default")
 
       messageType = header["message_type"]
@@ -18,17 +18,17 @@ def validateMessage(message):
 
       if schemas is None:
             raise Exception("Unknown schema version: {}".format(ver))
-      
+
       schema = schemas.getSchemaForMessageType(messageType)
 
-      validateMessageWithSchema(message, schema)
-      
-                            
-def validateMessageWithSchema(obj, schema):
-  
+      validateMessageWithSchema(message, schema, ver)
+
+
+def validateMessageWithSchema(obj, schema, ver):
+
       v = Validator(schema, allow_unknown=False)
       res = v.validate(obj)
 
       if res is not True:
-            raise Exception(str(v.errors))
-  
+            raise Exception("Errors with schema ver {}: {}".format(ver, v.errors))
+
